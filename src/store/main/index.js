e07f0c401928742c8b9a050f22c6fb7a87ff7568
lev <Angelina&25112011>
2021-06-25 15:13:28 +0300

    added logic for getting current location data(single, five days) working on vuex

--- src/store/main/index.js
+++ src/store/main/index.js
@@ -5,7 +5,16 @@ export default {
     namespaced: true,
 
     state: {
-        url: 'test',
+        urlSingle: '/currentconditions/v1/',
+        urlFiveDays: '/forecasts/v1/daily/5day/',
+        url: process.env.VUE_APP_MAIN_URL,
+        apiKey: process.env.VUE_APP_API_KEY,
+        currentLocationId: 215854,
+        currentLocationData: {
+            single:[],
+            fiveDays:{}
+        },
+        language: 'en-us'
     },
 
     actions,
