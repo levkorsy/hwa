5582f181ac563f971ff5ea36aad7e32fee56dca9
Gabriel <gabi@gnm.local>
2021-06-28 11:33:49 +0300

    move to bulma

--- src/components/navigation/Navbar.vue
+++ src/components/navigation/Navbar.vue
@@ -1,36 +1,64 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src="../../assets/logo.png" alt="4 seasons"  class="d-inline-block align-text-top"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
+<!--  <nav class="navbar navbar-expand-lg navbar-light bg-light">-->
+<!--    <div class="container-fluid">-->
+<!--      <a class="navbar-brand" href="#">-->
+<!--        <img src="../../assets/logo.png" alt="4 seasons"  class="d-inline-block align-text-top"></a>-->
+<!--      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"-->
+<!--              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--        <span class="navbar-toggler-icon"></span>-->
+<!--      </button>-->
+<!--      <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+<!--        <ul class="navbar-nav me-auto mb-2 mb-lg-0">-->
+<!--          <li class="nav-item">-->
+<!--            <router-link-->
+<!--                class="nav-link active"-->
+<!--                exact-->
+<!--                to="/"-->
+<!--            >-->
+<!--              Home-->
+<!--            </router-link>-->
+<!--          </li>-->
+<!--          <li class="nav-item">-->
+<!--            <router-link-->
+<!--                class="nav-link"-->
+<!--                exact-->
+<!--                to="/favorites"-->
+<!--            >-->
+<!--              Favorites-->
+<!--            </router-link>-->
+<!--          </li>-->
+
+<!--        </ul>-->
+
+<!--      </div>-->
+<!--    </div>-->
+<!--  </nav>-->
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand nav-layout">
+                  <router-link
+                      class="navbar-item"
+                      exact
+                      to="/"
+                  >
+                    <img src="../../assets/logo.png" alt="4 seasons">
+                  </router-link>
+
+          <div class="navbar-item">
             <router-link
-                class="nav-link active"
+                class="navbar-item"
                 exact
                 to="/"
             >
-              Home
+              HOME
             </router-link>
-          </li>
-          <li class="nav-item">
             <router-link
-                class="nav-link"
+                class="navbar-item"
                 exact
                 to="/favorites"
             >
-              Favorites
+              FAVORITES
             </router-link>
-          </li>
-
-        </ul>
-
-      </div>
+          </div>
     </div>
   </nav>
 </template>
@@ -42,5 +70,11 @@ export default {
 </script>
 
 <style scoped>
-
+.navbar-item img {
+  max-height: 3.75rem;
+}
+.nav-layout{
+  width: 100%;
+  justify-content: space-between;
+}
 </style>
